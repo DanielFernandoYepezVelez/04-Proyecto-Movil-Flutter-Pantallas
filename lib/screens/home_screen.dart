33fb646312dfead4f873a554c166a3a64d95f868
danielfernandoyepezvelez <danielfernandoyepezvelez>
2021-05-15 22:25:31 -0500

    Terminando El Ultimo Diseño De La Seccion

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'package:disenos/widgets/background.dart';
 import 'package:disenos/widgets/titles.dart';
+import 'package:disenos/widgets/card_table.dart';
 import 'package:disenos/widgets/bottom_navigation.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -23,7 +24,7 @@ class _HomeBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
         child: Column(
-      children: [Titles()],
+      children: [Titles(), CardTable()],
     ));
   }
 }
